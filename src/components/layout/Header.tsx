6bbfa411755f2a2785af07ccb8cbea86f4180205
hyeonjy <86361624+hyeonjy@users.noreply.github.com>
2025-06-09 22:25:18 +0900

    feat: 상단 고정 헤더 컴포넌트 구현

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,32 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import AuthButtons from './AuthButtons';
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="fixed left-0 top-0 w-full border-b border-[#cecece] bg-white">
+      <div className="flex h-16 w-full max-w-[1200px] items-center justify-between px-5 md:px-10 xl:mx-auto xl:px-0">
+        <div className="flex items-center">
+          <Link href="/">
+            <Image
+              src="/assets/logo.svg"
+              alt="LocalEat 로고"
+              width={100}
+              height={32}
+            />
+          </Link>
+
+          <nav className="ml-[40px] flex space-x-[32px] text-sm font-medium text-gray-800">
+            <Link href="#">이벤트</Link>
+            <Link href="/localeat">로컬잇</Link>
+            <Link href="/community">커뮤니티</Link>
+          </nav>
+        </div>
+
+        <AuthButtons />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
