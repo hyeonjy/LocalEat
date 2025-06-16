b98818fd04f6504ea00f7e64002c34f5ce411264
hyeonjy <86361624+hyeonjy@users.noreply.github.com>
2025-06-17 01:19:12 +0900

    feat: 리뷰작성 버튼 클릭 시 리뷰유형 선택 모달 표시

---
+++ src/app/restaurant/_components/ReviewWriteButton.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
+
+const ReviewWriteButton = ({ restaurantId }: { restaurantId: number }) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <button
+        onClick={() => setOpen(true)}
+        className="flex w-full items-center justify-center rounded-[9px] border border-[#9E9D9D] px-[16px] py-[12px] text-label-lmb"
+      >
+        <span className="mr-[7px]">리뷰 작성하기</span>
+        <Image
+          src="/assets/icons/arrow_back_ios_new.svg"
+          alt="오른쪽 화살표 아이콘"
+          width={20}
+          height={20}
+        />
+      </button>
+
+      {/* 모달 컴포넌트 */}
+      {open && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center gap-[30px] bg-[rgba(52,52,52,0.8)]">
+          <Link
+            href={`/restaurant/${restaurantId}/review/standard`}
+            className="shadow-inset-lg flex h-[470px] w-[350px] flex-col items-center justify-center rounded-[40px] bg-white"
+          >
+            <Image
+              src={'/assets/icons/standard.svg'}
+              alt={'일반 리뷰 작성 아이콘'}
+              width={213}
+              height={196}
+              className="h-[196px] w-[213px]"
+            />
+            <h2 className="text-title-lb">일반 리뷰 작성하기</h2>
+          </Link>
+          <Link
+            href={`/restaurant/${restaurantId}/review/graphic`}
+            className="shadow-inset-lg flex h-[470px] w-[350px] flex-col items-center justify-center rounded-[40px] bg-white"
+          >
+            <Image
+              src={'/assets/icons/graphic.svg'}
+              alt={'스토리카드 작성 아이콘'}
+              width={179}
+              height={196}
+              className="h-[196px] w-[179px]"
+            />
+            <h2 className="text-title-lb">스토리 리뷰 작성하기</h2>
+          </Link>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default ReviewWriteButton;
