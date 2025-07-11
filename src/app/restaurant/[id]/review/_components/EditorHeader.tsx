4471ebd2a983f8bc379a3b2afa2a7f7553bfd938
hyeonjy <86361624+hyeonjy@users.noreply.github.com>
2025-07-11 14:10:36 +0900

    feat: 이미지 업로드 및 캔버스 배경 설정 기능 구현

--- src/app/restaurant/[id]/review/_components/EditorHeader.tsx
+++ src/app/restaurant/[id]/review/_components/EditorHeader.tsx
@@ -1,7 +1,11 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-const EditorHeader = () => {
+interface EditorHeaderProps {
+  onImageUpload: () => void;
+}
+
+const EditorHeader = ({ onImageUpload }: EditorHeaderProps) => {
   return (
     <header className="fixed left-0 top-0 z-[999] flex w-full justify-center border-b border-[#cecece] bg-white">
       <div className="flex h-[82px] w-full items-center justify-between px-5 xl:w-[1200px]">
@@ -15,8 +19,17 @@ const EditorHeader = () => {
         </Link>
 
         <div className="flex gap-x-3">
-          <button className="h-[50px] rounded-[10px] border border-[#FA4D09] px-[24px] py-[10px] text-[20px] font-semibold leading-[150%] text-[#FA4D09]">
-            이미지 업로드
+          <button
+            onClick={onImageUpload}
+            className="flex h-[50px] gap-2 rounded-[10px] border border-[#FA4D09] px-[24px] py-[10px] text-[20px] font-semibold leading-[150%] text-[#FA4D09]"
+          >
+            <Image
+              src="/assets/icons/image.svg"
+              alt="이미지 업로드 버튼"
+              width={24}
+              height={24}
+            />
+            <p>이미지 업로드</p>
           </button>
           <button className="h-[50px] rounded-[10px] border bg-[#FA4D09] px-[24px] py-[10px] text-[20px] font-semibold leading-[150%] text-white">
             등록
